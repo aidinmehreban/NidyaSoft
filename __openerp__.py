b47f02a95d5f35534e074a7e9ce6440cae7ee8ff
aidinmehreban <aydin714@gmail.com>
2016-04-14 02:55:40 +0430

    Added files via upload

---
+++ __openerp__.py
@@ -0,0 +1,30 @@
+{
+    'name': 'NidyaSoft theme',
+    'description': 'A Pro Theme for ISP Companies.',
+    'version': '3.3',
+    'description': """
+===================================================
+""",
+    'author': 'Nidyasoft',
+    'maintainer': 'Aydin Mirzaie',
+    'website': 'http://www.nidyasoft.com',
+
+    'data': [
+        'style.xml',
+        #'web_menu_hide.xml'
+    ],
+    'category': 'Theme/Creative',
+    'depends': ['web','website'],
+    'js': [
+        #'static/src/js/web_menu_hide.js'
+    ],
+    'css': [
+        #'static/src/css/web_menu_hide.css',
+    ],
+    'css': [
+        'static/src/css/nidyasoft.css'
+    ],
+    'application': True,
+    'installable': True,
+
+}
